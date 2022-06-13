f60ada1504b5bea9f3637fd61d0a87eb954989df
lepene@gmail.com <lepene@gmail.com>
2022-06-13 14:07:35 +0200

    ok

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ from pytube import Playlist
 from pytube import Channel
 
 
+def print_playlists_channel(channel_url):
+    url_list = get_playlists_channel(channel_url)
+    for url in url_list:
+        print(url)
+    print("OK")
+
+
 def print_playlist(playlist):
     p = Playlist(playlist)
     if bool(p):
         print(p.title)
         for url in p.video_urls:
             print(url)
+        print("OK")
 
 
-def download_channel(channel_url, channel_name):
+def get_playlists_channel(channel_url):
+    url_list = []
     start = "playlist?list="
     end = '"'
     start_pos = 1
@@ -24,28 +33,52 @@ def download_channel(channel_url, channel_name):
         if start_pos >= start_pos_previous:
             end_pos = html.find(end, start_pos)
             url = f"https://www.youtube.com/playlist?list={html[start_pos:end_pos]}"
+            url_list.append(url)
+            start_pos_previous = start_pos
+    return url_list
+
+
+def download_channel(channel_url, channel_name):
+    url_list = get_playlists_channel(channel_url)
+    if bool(url_list):
+        for url in url_list:
             print(url)
             download_playlist(url, f"/home/jll/Downloads/{channel_name}/")
-            start_pos_previous = start_pos
+    # start = "playlist?list="
+    # end = '"'
+    # start_pos = 1
+    # end_pos = 1
+    # start_pos_previous = 0
+    # c = Channel(channel_url)
+    # html = c.playlists_html
+    # while start_pos >= start_pos_previous:
+    #     start_pos = html.find(start, start_pos) + len(start)
+    #     if start_pos >= start_pos_previous:
+    #         end_pos = html.find(end, start_pos)
+    #         url = f"https://www.youtube.com/playlist?list={html[start_pos:end_pos]}"
+    #         print(url)
+    #         download_playlist(url, f"/home/jll/Downloads/{channel_name}/")
+    #         start_pos_previous = start_pos
 
 
-def download_playlist(playlist, path):
-    p = Playlist(playlist)
+def download_playlist(playlist_url, out_path):
+    p = Playlist(playlist_url)
     if bool(p):
         print(p.title)
         i = 0
         for url in p.video_urls:
             i = i + 1
-            download_video(url, f"{path}{p.title}/", i)
+            download_video(url, f"{out_path}{p.title}/", i)
         print("Fin de liste.")
 
 
-def download_video(url, path, order):
-    video = YouTube(url)
-    print(video.title)
-    video.register_on_progress_callback(on_progress_callback)
-    video.register_on_complete_callback(on_download_complete)
-    video.streams.get_highest_resolution().download(output_path=f"{path}", filename_prefix=f"{str(order)}-")
+def download_video(video_url, out_path, order):
+    video = YouTube(video_url)
+    if bool(video):
+        print(video.title)
+        video.register_on_progress_callback(on_progress_callback)
+        video.register_on_complete_callback(on_download_complete)
+        video.streams.get_highest_resolution().download(output_path=f"{out_path}", filename_prefix=f"{str(order)}-")
 
 
 def on_progress_callback(stream, chunk, bytes_remaining):
@@ -60,4 +93,5 @@ def on_download_complete(stream, file_path):
 if __name__ == '__main__':
     channel_url = "https://www.youtube.com/c/Docstring/playlists"
     channel_name = "Docstrings"
-    download_channel(channel_url, channel_name)
+    print_playlists_channel(channel_url)
+#    download_channel(channel_url, channel_name)
